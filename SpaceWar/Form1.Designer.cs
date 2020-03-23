eaf0444b26b7f652719d7f83713852ab3cab7853
Oleg Butaev <oleg.butaew@yandex.ru>
2020-03-23 09:53:03 +0300

    Создалаи внешний вид игры

--- SpaceWar/Form1.Designer.cs
+++ SpaceWar/Form1.Designer.cs
@@ -28,13 +28,53 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBoxMyShip = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxMyShip)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBoxMyShip
+            // 
+            this.pictureBoxMyShip.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxMyShip.Image")));
+            this.pictureBoxMyShip.Location = new System.Drawing.Point(245, 306);
+            this.pictureBoxMyShip.Name = "pictureBoxMyShip";
+            this.pictureBoxMyShip.Size = new System.Drawing.Size(91, 108);
+            this.pictureBoxMyShip.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxMyShip.TabIndex = 0;
+            this.pictureBoxMyShip.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(194, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(91, 108);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pictureBoxMyShip);
+            this.Name = "Form1";
+            this.Text = "Космические войны";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxMyShip)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoxMyShip;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
